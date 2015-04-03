3251b1be4b7338758f7c032434656791d7af9c40
Mallika Kulkarni <mallikak2709@gmail.com>
2015-04-03 13:56:18 -0700

    Initial project commit

---
+++ Project281/src/main/java/com/project281/v1/Application.java
@@ -0,0 +1,30 @@
+package com.project281.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+/**
+ * Created by mallika on 4/3/15.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
---
+++ Project281/src/test/java/com/project281/v1/ApplicationTest.java
@@ -0,0 +1,25 @@
+package com.project281.v1;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ApplicationTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGreeting() throws Exception {
+
+    }
+}
